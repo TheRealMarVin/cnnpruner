1ffdebd4b677ad63004e8bd1f582af1fa705c287
therealmarvin <nakedtoothbrush@hotmail.com>
2019-04-13 09:38:32 -0400

    fixing issue with progress graph and connection count.. .damn forking in graph

--- GraphHelper.py
+++ GraphHelper.py
@@ -11,15 +11,15 @@ def get_childs(graph_edges, parent_name):
     return res
 
 
-def get_parents(graph_edges, child_name):
-    result = []
-    for parent_name, values in graph_edges.items():
-        for name, _ in values:
-            if name == child_name:
-                if name in get_childs(graph_edges, parent_name):
-                    result.append(parent_name)
-                    break
-    return result
+# def get_parents(graph_edges, child_name):
+#     result = []
+#     for parent_name, values in graph_edges.items():
+#         for name, _ in values:
+#             if name == child_name:
+#                 if name in get_childs(graph_edges, parent_name):
+#                     result.append(parent_name)
+#                     break
+#     return result
 
 # def get__input_connection_count_per_entry(graph_edges, node, res):
 #     if node in graph_edges:
@@ -30,46 +30,56 @@ def get_parents(graph_edges, child_name):
 #                 res[name] = 1
 #             get__input_connection_count_per_entry(graph_edges, name, res)
 
-def generate_graph_old(model, args):
-    graph = {}
+# def generate_graph_old(model, args):
+#     graph = {}
+#
+#     # Run the Pytorch graph to get a trace and generate a graph from it
+#     trace, out = torch.jit.get_trace_graph(model, args)
+#     torch.onnx._optimize_trace(trace, torch.onnx.OperatorExportTypes.ONNX)
+#     torch_graph = trace.graph()
+#
+#     model_name = model._get_name()
+#     root = None
+#     for torch_node in torch_graph.nodes():
+#         inputs = [o.unique() for o in torch_node.inputs()]
+#         outputs = [o.unique() for o in torch_node.outputs()]
+#
+#         # Get output shape
+#         shape = get_shape(torch_node)
+#
+#         # Add edges
+#         curr_name = reformat_path(model_name, torch_node.scopeName())
+#         sub_layers = []
+#         for target_torch_node in torch_graph.nodes():
+#             target_inputs = [i.unique() for i in target_torch_node.inputs()]
+#             target_outputs = [o.unique() for o in target_torch_node.outputs()]
+#             target_name = reformat_path(model_name, target_torch_node.scopeName())
+#
+#             intersect = set(outputs) & set(target_inputs)
+#             if intersect and shape is not None:
+#                 if curr_name == "":
+#                     curr_name = str(inputs)
+#                 if target_name == "":
+#                     target_name = str(target_inputs)
+#                 if root is None:
+#                     root = curr_name #TODO this may be absolutely wrong
+#                 # print("Line: \n\tcurr: {} \n\tnext: {}\n\tshape:{}".format(curr_name, target_name, shape))
+#                 print("Line: intersect:{} ci{} co{} ti{} to{}\n\tcurr: {} \n\tnext: {}\n\tshape:{}".format(intersect, inputs, outputs, target_inputs, target_outputs, curr_name, target_name, shape))
+#                 sub_layers.append((target_name, shape))
+#
+#         if len(sub_layers) > 0:
+#             graph[curr_name] = sub_layers
+#     return graph, root
 
-    # Run the Pytorch graph to get a trace and generate a graph from it
-    trace, out = torch.jit.get_trace_graph(model, args)
-    torch.onnx._optimize_trace(trace, torch.onnx.OperatorExportTypes.ONNX)
-    torch_graph = trace.graph()
-
-    model_name = model._get_name()
-    root = None
-    for torch_node in torch_graph.nodes():
-        inputs = [o.unique() for o in torch_node.inputs()]
-        outputs = [o.unique() for o in torch_node.outputs()]
-
-        # Get output shape
-        shape = get_shape(torch_node)
-
-        # Add edges
-        curr_name = reformat_path(model_name, torch_node.scopeName())
-        sub_layers = []
-        for target_torch_node in torch_graph.nodes():
-            target_inputs = [i.unique() for i in target_torch_node.inputs()]
-            target_outputs = [o.unique() for o in target_torch_node.outputs()]
-            target_name = reformat_path(model_name, target_torch_node.scopeName())
-
-            intersect = set(outputs) & set(target_inputs)
-            if intersect and shape is not None:
-                if curr_name == "":
-                    curr_name = str(inputs)
-                if target_name == "":
-                    target_name = str(target_inputs)
-                if root is None:
-                    root = curr_name #TODO this may be absolutely wrong
-                # print("Line: \n\tcurr: {} \n\tnext: {}\n\tshape:{}".format(curr_name, target_name, shape))
-                print("Line: intersect:{} ci{} co{} ti{} to{}\n\tcurr: {} \n\tnext: {}\n\tshape:{}".format(intersect, inputs, outputs, target_inputs, target_outputs, curr_name, target_name, shape))
-                sub_layers.append((target_name, shape))
-
-        if len(sub_layers) > 0:
-            graph[curr_name] = sub_layers
-    return graph, root
+def get__input_connection_count_per_entry(graph_edges, node, res):
+    if node in graph_edges:
+        for name in graph_edges[node].split(","):
+            if name in res.keys():
+                res[name] += 1
+                break
+            else:
+                res[name] = 1
+            get__input_connection_count_per_entry(graph_edges, name, res)
 
 def generate_graph(model, args):
     execution_graph = {}
@@ -101,18 +111,19 @@ def generate_graph(model, args):
             intersect = set(outputs) & set(target_inputs)
             if intersect and shape is not None:
                 if curr_name == "":
-                    curr_name = str(inputs)
+                    curr_name = ",".join(str(x) for x in inputs)
                 # if target_name == "":
                 #     target_name = str(target_inputs)
+                intersect_as_string = ",".join(str(x) for x in intersect)
                 if root is None:
-                    root = str(intersect)
+                    root = intersect_as_string
                 # print("Line: \n\tcurr: {} \n\tnext: {}\n\tshape:{}".format(curr_name, target_name, shape))
                 # print("Line: intersect:{} ci{} co{} ti{} to{}\n\tcurr: {} \n\tshape:{}".format(intersect, inputs, outputs, target_inputs, target_outputs, curr_name, shape))
-                if str(intersect) in execution_graph:
-                    execution_graph[str(intersect)].extend(np.array(target_outputs))
+                if intersect_as_string in execution_graph:
+                    execution_graph[intersect_as_string].extend(np.array(target_outputs))
                 else:
-                    execution_graph[str(intersect)] = target_outputs
-                id_name_dict[str(intersect)] = curr_name
+                    execution_graph[intersect_as_string] = target_outputs
+                id_name_dict[intersect_as_string] = curr_name
 
     execution_graph = clean_execution_graph(execution_graph)
     return execution_graph, id_name_dict, root
@@ -121,14 +132,14 @@ def clean_execution_graph(execution_graph):
     cleaned_graph = {}
     for k, v in execution_graph.items():
         for i in v:
-            if str(set([i])) in execution_graph:
+            if str(i) in execution_graph:
                 if k not in cleaned_graph:
                     cleaned_graph[k] = [i]
                 else:
                     cleaned_graph[k].append(i)
 
     for k, v in cleaned_graph.items():
-        cleaned_graph[k] = str(set(cleaned_graph[k]))
+        cleaned_graph[k] = ",".join(str(x) for x in cleaned_graph[k])
     return cleaned_graph
 
 def reformat_path(model_name, entry):
