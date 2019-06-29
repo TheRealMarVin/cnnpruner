e13bbd47f79dfdd4d0316f3538b9ee09bc190c8c
therealmarvin <nakedtoothbrush@hotmail.com>
2019-06-28 21:04:11 -0400

    preparing some cleanup

--- SandBox.py
+++
@@ -1,8 +0,0 @@
-import torch
-
-# to draw network: http://alexlenail.me/NN-SVG/AlexNet.html
-
-if __name__ == '__main__':
-    print("hello")
-    a = torch.load("saved/alex0.3/PrunedAlexnet.pth")
-    b = 0
