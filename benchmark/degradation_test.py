df0493a9a6dbd885969f8ee8084c9f26590b4491
therealmarvin <nakedtoothbrush@hotmail.com>
2019-07-03 21:52:36 -0400

    probably good for first tests on degradation

--- benchmark/degradation_test.py
+++ benchmark/degradation_test.py
@@ -1,7 +1,11 @@
+import numpy as np
 from torchvision.datasets import CIFAR10
 from torchvision.transforms import transforms
 
+import matplotlib.pyplot as plt
+
 from POC import DatasetParams, PruningParams, ExecParams
+from Pruner.CompletePruning.CompleteTaylorExpansionFilterPruner import CompleteTaylorExpansionFilterPruner
 from benchmark.BenchmarkHelper import exec_squeeze_net, exec_alexnet, exec_resnet18, exec_resnet34, exec_resnet50, \
     exec_dense_net, exec_vgg16
 from deeplib_ext.MultiHistory import MultiHistory
@@ -16,117 +20,131 @@ def train_models(dataset_params):
     multi_history = MultiHistory()
 
     exec_name = "AlexNet-base"
-    h = exec_alexnet(exec_name, pruning_params=pruning_param_no_prune, exec_params=exec_param_no_prune,
-                         dataset_params=dataset_params)
+    h, s = exec_alexnet(exec_name, pruning_params=pruning_param_no_prune, exec_params=exec_param_no_prune,
+                        dataset_params=dataset_params)
     multi_history.append_history(exec_name, h)
     multi_history.display_single_key(History.VAL_ACC_KEY, title="Models training without pruning")
 
     exec_name = "ResNet18-base"
-    h = exec_resnet18(exec_name, pruning_params=pruning_param_no_prune, exec_params=exec_param_no_prune,
-                     dataset_params=dataset_params)
+    h, s = exec_resnet18(exec_name, pruning_params=pruning_param_no_prune, exec_params=exec_param_no_prune,
+                         dataset_params=dataset_params)
     multi_history.append_history(exec_name, h)
     multi_history.display_single_key(History.VAL_ACC_KEY, title="Models training without pruning")
 
     exec_name = "ResNet34-base"
-    h = exec_resnet34(exec_name, pruning_params=pruning_param_no_prune, exec_params=exec_param_no_prune,
-                      dataset_params=dataset_params)
+    h, s = exec_resnet34(exec_name, pruning_params=pruning_param_no_prune, exec_params=exec_param_no_prune,
+                         dataset_params=dataset_params)
     multi_history.append_history(exec_name, h)
     multi_history.display_single_key(History.VAL_ACC_KEY, title="Models training without pruning")
 
     exec_name = "ResNet50-base"
-    h = exec_resnet50(exec_name, pruning_params=pruning_param_no_prune, exec_params=exec_param_no_prune,
-                      dataset_params=dataset_params)
+    h, s = exec_resnet50(exec_name, pruning_params=pruning_param_no_prune, exec_params=exec_param_no_prune,
+                         dataset_params=dataset_params)
     multi_history.append_history(exec_name, h)
     multi_history.display_single_key(History.VAL_ACC_KEY, title="Models training without pruning")
 
     exec_name = "Squeeze-base"
-    h = exec_squeeze_net(exec_name, pruning_params=pruning_param_no_prune, exec_params=exec_param_no_prune,
-                         dataset_params=dataset_params)
+    h, s = exec_squeeze_net(exec_name, pruning_params=pruning_param_no_prune, exec_params=exec_param_no_prune,
+                            dataset_params=dataset_params)
     multi_history.append_history(exec_name, h)
     multi_history.display_single_key(History.VAL_ACC_KEY, title="Models training without pruning")
 
     exec_name = "DenseNet121-base"
-    h = exec_dense_net(exec_name, pruning_params=pruning_param_no_prune, exec_params=exec_param_no_prune,
-                         dataset_params=dataset_params)
+    h, s = exec_dense_net(exec_name, pruning_params=pruning_param_no_prune, exec_params=exec_param_no_prune,
+                          dataset_params=dataset_params)
     multi_history.append_history(exec_name, h)
     multi_history.display_single_key(History.VAL_ACC_KEY, title="Models training without pruning")
 
     exec_name = "VGG16-base"
-    h = exec_vgg16(exec_name, pruning_params=pruning_param_no_prune, exec_params=exec_param_no_prune,
-                       dataset_params=dataset_params)
+    h, s = exec_vgg16(exec_name, pruning_params=pruning_param_no_prune, exec_params=exec_param_no_prune,
+                      dataset_params=dataset_params)
     multi_history.append_history(exec_name, h)
     multi_history.display_single_key(History.VAL_ACC_KEY, title="Models training without pruning")
 
 
 def run_compare_pruning(dataset_params):
     pruning_param_no_prune = PruningParams(max_percent_per_iteration=0.0, prune_ratio=0.0)
-    exec_param = ExecParams(n_pretrain_epoch=0, n_epoch_retrain=3, n_epoch_total=3, batch_size=32, pruner=None)
+    exec_param = ExecParams(n_pretrain_epoch=0, n_epoch_retrain=3, n_epoch_total=3, batch_size=32,
+                            pruner=CompleteTaylorExpansionFilterPruner)
 
-    multi_history = MultiHistory()
-
+    all_scores = []
     exec_name = "AlexNet-degrad"
-    multi_history_local = MultiHistory()
+    score = []
     exec_param.best_result_save_path = "../saved/AlexNet-base/Pruned.pth".format(exec_name)
     exec_param.retrain_if_weight_loaded = True
-    for i in range(0, 3):
+    for i in range(0, 11):
         desired_pruning = (5.0 * i)/100.0
-        pruning_param_no_prune.max_percent_per_iteration = desired_pruning
-        pruning_param_no_prune.prune_ratio = desired_pruning
-        h = exec_alexnet(exec_name, pruning_params=pruning_param_no_prune, exec_params=exec_param,
-                         dataset_params=dataset_params)
-        multi_history.append_history(exec_name, h)
-        multi_history_local.append_history(exec_name, h)
+        if desired_pruning != 0.0:
+            pruning_param_no_prune.max_percent_per_iteration = desired_pruning
+            pruning_param_no_prune.prune_ratio = desired_pruning
+        else:
+            pruning_param_no_prune.max_percent_per_iteration = None
+            pruning_param_no_prune.prune_ratio = None
+        h, s = exec_alexnet(exec_name, pruning_params=pruning_param_no_prune, exec_params=exec_param,
+                     dataset_params=dataset_params)
+        score.append(s)
 
-    multi_history_local.display_single_key(History.VAL_ACC_KEY, title="Various pruning on AlexNet")
-    multi_history.display_single_key(History.VAL_ACC_KEY, title="Various pruning degradation")
+    all_scores.append(score)
+    display_graphs(score, all_scores)
+
 
     exec_name = "ResNet18-degrad"
-    multi_history_local = MultiHistory()
+    score = []
     exec_param.best_result_save_path = "../saved/ResNet18-base/Pruned.pth".format(exec_name)
     exec_param.retrain_if_weight_loaded = True
     for i in range(0, 11):
         desired_pruning = (5.0 * i)/100.0
-        pruning_param_no_prune.max_percent_per_iteration = desired_pruning
-        pruning_param_no_prune.prune_ratio = desired_pruning
-        h = exec_resnet18(exec_name, pruning_params=pruning_param_no_prune, exec_params=exec_param,
-                          dataset_params=dataset_params)
-        multi_history.append_history(exec_name, h)
-        multi_history_local.append_history(exec_name, h)
+        if desired_pruning != 0.0:
+            pruning_param_no_prune.max_percent_per_iteration = desired_pruning
+            pruning_param_no_prune.prune_ratio = desired_pruning
+        else:
+            pruning_param_no_prune.max_percent_per_iteration = None
+            pruning_param_no_prune.prune_ratio = None
+        h, s = exec_resnet18(exec_name, pruning_params=pruning_param_no_prune, exec_params=exec_param,
+                             dataset_params=dataset_params)
+        score.append(s)
 
-    multi_history_local.display_single_key(History.VAL_ACC_KEY, title="Various pruning on ResNet18")
-    multi_history.display_single_key(History.VAL_ACC_KEY, title="Various pruning on degradation")
+    all_scores.append(score)
+    display_graphs(score, all_scores)
 
     exec_name = "ResNet34-degrad"
-    multi_history_local = MultiHistory()
+    score = []
     exec_param.best_result_save_path = "../saved/ResNet34-base/Pruned.pth".format(exec_name)
     exec_param.retrain_if_weight_loaded = True
     for i in range(0, 11):
         desired_pruning = (5.0 * i)/100.0
-        pruning_param_no_prune.max_percent_per_iteration = desired_pruning
-        pruning_param_no_prune.prune_ratio = desired_pruning
-        h = exec_resnet34(exec_name, pruning_params=pruning_param_no_prune, exec_params=exec_param,
-                          dataset_params=dataset_params)
-        multi_history.append_history(exec_name, h)
-        multi_history_local.append_history(exec_name, h)
+        if desired_pruning != 0.0:
+            pruning_param_no_prune.max_percent_per_iteration = desired_pruning
+            pruning_param_no_prune.prune_ratio = desired_pruning
+        else:
+            pruning_param_no_prune.max_percent_per_iteration = None
+            pruning_param_no_prune.prune_ratio = None
+        h, s = exec_resnet34(exec_name, pruning_params=pruning_param_no_prune, exec_params=exec_param,
+                             dataset_params=dataset_params)
+        score.append(s)
 
-    multi_history_local.display_single_key(History.VAL_ACC_KEY, title="Various pruning on ResNet34")
-    multi_history.display_single_key(History.VAL_ACC_KEY, title="Various pruning on degradation")
+    all_scores.append(score)
+    display_graphs(score, all_scores)
+
 
     exec_name = "ResNet34-degrad"
-    multi_history_local = MultiHistory()
+    score = []
     exec_param.best_result_save_path = "../saved/ResNet34-base/Pruned.pth".format(exec_name)
     exec_param.retrain_if_weight_loaded = True
     for i in range(0, 11):
         desired_pruning = (5.0 * i)/100.0
-        pruning_param_no_prune.max_percent_per_iteration = desired_pruning
-        pruning_param_no_prune.prune_ratio = desired_pruning
-        h = exec_resnet34(exec_name, pruning_params=pruning_param_no_prune, exec_params=exec_param,
-                          dataset_params=dataset_params)
-        multi_history.append_history(exec_name, h)
-        multi_history_local.append_history(exec_name, h)
+        if desired_pruning != 0.0:
+            pruning_param_no_prune.max_percent_per_iteration = desired_pruning
+            pruning_param_no_prune.prune_ratio = desired_pruning
+        else:
+            pruning_param_no_prune.max_percent_per_iteration = None
+            pruning_param_no_prune.prune_ratio = None
+        h, s = exec_resnet34(exec_name, pruning_params=pruning_param_no_prune, exec_params=exec_param,
+                             dataset_params=dataset_params)
+        score.append(s)
 
-    multi_history_local.display_single_key(History.VAL_ACC_KEY, title="Various pruning on ResNet34")
-    multi_history.display_single_key(History.VAL_ACC_KEY, title="Various pruning on degradation")
+    all_scores.append(score)
+    display_graphs(score, all_scores)
 
     exec_name = "ResNet50-degrad"
     multi_history_local = MultiHistory()
@@ -134,63 +152,91 @@ def run_compare_pruning(dataset_params):
     exec_param.retrain_if_weight_loaded = True
     for i in range(0, 11):
         desired_pruning = (5.0 * i)/100.0
-        pruning_param_no_prune.max_percent_per_iteration = desired_pruning
-        pruning_param_no_prune.prune_ratio = desired_pruning
-        h = exec_resnet50(exec_name, pruning_params=pruning_param_no_prune, exec_params=exec_param,
-                          dataset_params=dataset_params)
-        multi_history.append_history(exec_name, h)
-        multi_history_local.append_history(exec_name, h)
+        if desired_pruning != 0.0:
+            pruning_param_no_prune.max_percent_per_iteration = desired_pruning
+            pruning_param_no_prune.prune_ratio = desired_pruning
+        else:
+            pruning_param_no_prune.max_percent_per_iteration = None
+            pruning_param_no_prune.prune_ratio = None
+        h, s = exec_resnet50(exec_name, pruning_params=pruning_param_no_prune, exec_params=exec_param,
+                             dataset_params=dataset_params)
+        score.append(s)
 
-    multi_history_local.display_single_key(History.VAL_ACC_KEY, title="Various pruning on ResNet50")
-    multi_history.display_single_key(History.VAL_ACC_KEY, title="Various pruning on degradation")
+    all_scores.append(score)
+    display_graphs(score, all_scores)
 
     exec_name = "Squeeze-degrad"
-    multi_history_local = MultiHistory()
+    score = []
     exec_param.best_result_save_path = "../saved/Squeeze-base/Pruned.pth".format(exec_name)
     exec_param.retrain_if_weight_loaded = True
     for i in range(0, 11):
         desired_pruning = (5.0 * i)/100.0
-        pruning_param_no_prune.max_percent_per_iteration = desired_pruning
-        pruning_param_no_prune.prune_ratio = desired_pruning
-        h = exec_vgg16(exec_name, pruning_params=pruning_param_no_prune, exec_params=exec_param,
-                       dataset_params=dataset_params)
-        multi_history.append_history(exec_name, h)
-        multi_history_local.append_history(exec_name, h)
+        if desired_pruning != 0.0:
+            pruning_param_no_prune.max_percent_per_iteration = desired_pruning
+            pruning_param_no_prune.prune_ratio = desired_pruning
+        else:
+            pruning_param_no_prune.max_percent_per_iteration = None
+            pruning_param_no_prune.prune_ratio = None
+        h, s = exec_vgg16(exec_name, pruning_params=pruning_param_no_prune, exec_params=exec_param,
+                          dataset_params=dataset_params)
+        score.append(s)
 
-    multi_history_local.display_single_key(History.VAL_ACC_KEY, title="Various pruning on Squeeze")
-    multi_history.display_single_key(History.VAL_ACC_KEY, title="Various pruning on degradation")
+    all_scores.append(score)
+    display_graphs(score, all_scores)
 
     exec_name = "DenseNet121-degrad"
-    multi_history_local = MultiHistory()
+    score = []
     exec_param.best_result_save_path = "../saved/DenseNet121-base/Pruned.pth".format(exec_name)
     exec_param.retrain_if_weight_loaded = True
     for i in range(0, 11):
         desired_pruning = (5.0 * i)/100.0
-        pruning_param_no_prune.max_percent_per_iteration = desired_pruning
-        pruning_param_no_prune.prune_ratio = desired_pruning
-        h = exec_dense_net(exec_name, pruning_params=pruning_param_no_prune, exec_params=exec_param,
-                           dataset_params=dataset_params)
-        multi_history.append_history(exec_name, h)
-        multi_history_local.append_history(exec_name, h)
+        if desired_pruning != 0.0:
+            pruning_param_no_prune.max_percent_per_iteration = desired_pruning
+            pruning_param_no_prune.prune_ratio = desired_pruning
+        else:
+            pruning_param_no_prune.max_percent_per_iteration = None
+            pruning_param_no_prune.prune_ratio = None
+        h, s = exec_dense_net(exec_name, pruning_params=pruning_param_no_prune, exec_params=exec_param,
+                              dataset_params=dataset_params)
+        score.append(s)
 
-    multi_history_local.display_single_key(History.VAL_ACC_KEY, title="Various pruning on DenseNet121")
-    multi_history.display_single_key(History.VAL_ACC_KEY, title="Various pruning on degradation")
+    all_scores.append(score)
+    display_graphs(score, all_scores)
 
     exec_name = "VGG16-degrad"
-    multi_history_local = MultiHistory()
+    score = []
     exec_param.best_result_save_path = "../saved/VGG16-base/Pruned.pth".format(exec_name)
     exec_param.retrain_if_weight_loaded = True
     for i in range(0, 11):
         desired_pruning = (5.0 * i)/100.0
-        pruning_param_no_prune.max_percent_per_iteration = desired_pruning
-        pruning_param_no_prune.prune_ratio = desired_pruning
-        h = exec_dense_net(exec_name, pruning_params=pruning_param_no_prune, exec_params=exec_param,
-                           dataset_params=dataset_params)
-        multi_history.append_history(exec_name, h)
-        multi_history_local.append_history(exec_name, h)
+        if desired_pruning != 0.0:
+            pruning_param_no_prune.max_percent_per_iteration = desired_pruning
+            pruning_param_no_prune.prune_ratio = desired_pruning
+        else:
+            pruning_param_no_prune.max_percent_per_iteration = None
+            pruning_param_no_prune.prune_ratio = None
+        h, s = exec_dense_net(exec_name, pruning_params=pruning_param_no_prune, exec_params=exec_param,
+                              dataset_params=dataset_params)
+        score.append(s)
 
-    multi_history_local.display_single_key(History.VAL_ACC_KEY, title="Various pruning on VGG16")
-    multi_history.display_single_key(History.VAL_ACC_KEY, title="Various pruning on degradation")
+    all_scores.append(score)
+    display_graphs(score, all_scores)
+
+
+def display_graphs(current_score, all_scores):
+    plt.title("Display degradatrion")
+    # plt.xlabel(xlabel)
+    plt.ylabel("Pruning Percent")
+    display_graphs(current_score, all_scores)
+    plt.plot(current_score)
+    plt.show()
+
+    plt.title("Display full degradatrion")
+    # plt.xlabel(xlabel)
+    plt.ylabel("Pruning Percent")
+    g = np.transpose(all_scores)
+    plt.plot(g)
+    plt.show()
 
 
 if __name__ == '__main__':
